e65b6cf07419e5f299374a57314a077a70320dc6
suudi abdisalan <abdisalansucdi@gmail.com>
2024-09-25 21:37:58 +0300

    complete arrow lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function() {
+    return 2000 / 100;
+};
+console.log(divide());
+
+ const square = (x) => x * x;
+ console.log(square(4));
+ 
+
+ const add = (a, b) => a + b;
+ const result = add(3, 4);
+ console.log(result ===7, `Expected 7, but got ${result}`);
+ 
